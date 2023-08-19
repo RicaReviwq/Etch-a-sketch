0f4e5b49a52a1fd977e39c370a5510e47d2a8d91
RicaReviwq <rzgg654@gmail.com>
2023-08-18 20:20:56 -0600

    Replique los 4 primeros cuadrados, otras 4 veces para formar un total de 16 cuadrados

--- script.js
+++ script.js
@@ -1,25 +1,94 @@
 const cuadricula = document.getElementById("cuadricula");
 
+//TODOS LOS CUADRADOS DE LA CUADRICULA
 
+//CUADRADO1
 const cuadrado1 = document.createElement('div');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-cuadrado1.setAttribute("style", "width: 100px; height:200px; background: green;");
+cuadrado1.setAttribute("style", "width: 150px; height:150px; background: green; border:2px solid black;");
 cuadricula.appendChild(cuadrado1);
+
+//CUADRADO2
+const cuadrado2 = document.createElement('div');
+cuadrado2.setAttribute("style", "width: 150px; height:150px; background: red; border:2px solid black");
+cuadricula.appendChild(cuadrado2);
+
+//CUADRADO3
+const cuadrado3 = document.createElement('div');
+cuadrado3.setAttribute("style", "width: 150px; height:150px; background: blue; border:2px solid black");
+cuadricula.appendChild(cuadrado3);
+
+//CUADRADO4
+const cuadrado4 = document.createElement('div');
+cuadrado4.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado4);
+
+//CUADRADO5
+const cuadrado5 = document.createElement('div');
+cuadrado5.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado5);
+
+
+//CUADRADO6
+const cuadrado6 = document.createElement('div');
+cuadrado6.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado6);
+
+
+//CUADRADO7
+const cuadrado7 = document.createElement('div');
+cuadrado7.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado7);
+
+//CUADRADO8
+const cuadrado8 = document.createElement('div');
+cuadrado8.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado8);
+
+
+//CUADRADO9
+const cuadrado9 = document.createElement('div');
+cuadrado9.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado9);
+
+
+//CUADRADO10
+const cuadrado10 = document.createElement('div');
+cuadrado10.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado10);
+
+
+//CUADRADO11
+const cuadrado11 = document.createElement('div');
+cuadrado11.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado11);
+
+
+//CUADRADO12
+const cuadrado12 = document.createElement('div');
+cuadrado12.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado12);
+
+
+//CUADRADO13
+const cuadrado13 = document.createElement('div');
+cuadrado13.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado13);
+
+
+//CUADRADO14
+const cuadrado14 = document.createElement('div');
+cuadrado14.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado14);
+
+
+//CUADRADO15
+const cuadrado15 = document.createElement('div');
+cuadrado15.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado15);
+
+
+//CUADRADO16
+const cuadrado16 = document.createElement('div');
+cuadrado16.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadricula.appendChild(cuadrado16);
+
