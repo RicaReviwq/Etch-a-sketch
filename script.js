3ca5b508760638464830419678bb3ab4cf977dd4
RicaReviwq <rzgg654@gmail.com>
2023-08-18 19:37:17 -0600

    Creamos el primer cuadrado dentra de la cuadricula

---
+++ script.js
@@ -0,0 +1,25 @@
+const cuadricula = document.getElementById("cuadricula");
+
+
+const cuadrado1 = document.createElement('div');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cuadrado1.setAttribute("style", "width: 100px; height:200px; background: green;");
+cuadricula.appendChild(cuadrado1);
