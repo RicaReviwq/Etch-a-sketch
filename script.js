7aa93d0c0fab3dbfda1fe64838951221cc2da21f
RicaReviwq <rzgg654@gmail.com>
2023-08-18 20:45:25 -0600

    Elimine el evento 'mouseout' para que el color se mantuviera, y replique lo mismo con los 16 cuadrados restantes

--- script.js
+++ script.js
@@ -6,98 +6,168 @@ const cuadricula = document.getElementById("cuadricula");
 const cuadrado1 = document.createElement('div');
 cuadrado1.setAttribute("style", "width: 150px; height:150px; border:2px solid black;");
 cuadricula.appendChild(cuadrado1);
+//Eventos para cambiar el fondo
 cuadrado1.addEventListener('mouseover', ()=>{
     cuadrado1.style.background = "yellow";
 });
 
-cuadrado1.addEventListener('mouseout', ()=>{
-    cuadrado1.style.background = "none";
-})
 
 
 
 //CUADRADO2
 const cuadrado2 = document.createElement('div');
-cuadrado2.setAttribute("style", "width: 150px; height:150px; background: red; border:2px solid black");
+cuadrado2.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado2);
+
+cuadrado2.addEventListener('mouseover', ()=>{
+    cuadrado2.style.background = "yellow";
+});
+
+
 
 //CUADRADO3
 const cuadrado3 = document.createElement('div');
-cuadrado3.setAttribute("style", "width: 150px; height:150px; background: blue; border:2px solid black");
+cuadrado3.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado3);
+
+cuadrado3.addEventListener('mouseover', ()=>{
+    cuadrado3.style.background = "yellow";
+});
+
 
 //CUADRADO4
 const cuadrado4 = document.createElement('div');
-cuadrado4.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado4.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado4);
+
+cuadrado4.addEventListener('mouseover', ()=>{
+    cuadrado4.style.background = "yellow";
+});
+
 
 //CUADRADO5
 const cuadrado5 = document.createElement('div');
-cuadrado5.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado5.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado5);
+
+cuadrado5.addEventListener('mouseover', ()=>{
+    cuadrado5.style.background = "yellow";
+});
 
 
 //CUADRADO6
 const cuadrado6 = document.createElement('div');
-cuadrado6.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado6.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado6);
+
+cuadrado6.addEventListener('mouseover', ()=>{
+    cuadrado6.style.background = "yellow";
+});
+
 
 
 //CUADRADO7
 const cuadrado7 = document.createElement('div');
-cuadrado7.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado7.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado7);
+
+cuadrado7.addEventListener('mouseover', ()=>{
+    cuadrado7.style.background = "yellow";
+});
+
 
 //CUADRADO8
 const cuadrado8 = document.createElement('div');
-cuadrado8.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado8.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado8);
+
+cuadrado8.addEventListener('mouseover', ()=>{
+    cuadrado8.style.background = "yellow";
+});
 
 
 //CUADRADO9
 const cuadrado9 = document.createElement('div');
-cuadrado9.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado9.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado9);
+
+cuadrado9.addEventListener('mouseover', ()=>{
+    cuadrado9.style.background = "yellow";
+});
 
 
 //CUADRADO10
 const cuadrado10 = document.createElement('div');
-cuadrado10.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado10.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado10);
+
+cuadrado10.addEventListener('mouseover', ()=>{
+    cuadrado10.style.background = "yellow";
+});
+
 
 
 //CUADRADO11
 const cuadrado11 = document.createElement('div');
-cuadrado11.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado11.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado11);
+
+cuadrado11.addEventListener('mouseover', ()=>{
+    cuadrado11.style.background = "yellow";
+});
+
 
 
 //CUADRADO12
 const cuadrado12 = document.createElement('div');
-cuadrado12.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado12.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado12);
+
+cuadrado12.addEventListener('mouseover', ()=>{
+    cuadrado12.style.background = "yellow";
+});
+
 
 
 //CUADRADO13
 const cuadrado13 = document.createElement('div');
-cuadrado13.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado13.setAttribute("style", "width: 150px; height:150px;border:2px solid black");
 cuadricula.appendChild(cuadrado13);
+
+cuadrado13.addEventListener('mouseover', ()=>{
+    cuadrado13.style.background = "yellow";
+});
+
 
 
 //CUADRADO14
 const cuadrado14 = document.createElement('div');
-cuadrado14.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado14.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado14);
+
+cuadrado14.addEventListener('mouseover', ()=>{
+    cuadrado14.style.background = "yellow";
+});
+
 
 
 //CUADRADO15
 const cuadrado15 = document.createElement('div');
-cuadrado15.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado15.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado15);
+
+cuadrado15.addEventListener('mouseover', ()=>{
+    cuadrado15.style.background = "yellow";
+});
+
 
 
 //CUADRADO16
 const cuadrado16 = document.createElement('div');
-cuadrado16.setAttribute("style", "width: 150px; height:150px; background: orange; border:2px solid black");
+cuadrado16.setAttribute("style", "width: 150px; height:150px; border:2px solid black");
 cuadricula.appendChild(cuadrado16);
+
+cuadrado16.addEventListener('mouseover', ()=>{
+    cuadrado16.style.background = "yellow";
+});
 
