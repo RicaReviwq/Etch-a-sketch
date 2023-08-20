fff8d3aabf2af69f0c2aed50408f6c07fdaa07ce
RicaReviwq <rzgg654@gmail.com>
2023-08-20 09:28:42 -0600

    Puse un nuevo boton para reinicar el coloreado que haya hecho el usuario

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const cuadricula = document.getElementById("cuadricula");
+const REFRESCAR = document.getElementById("refrescar");
+
+REFRESCAR.addEventListener('click', () =>{
+    location.reload();
+});
  
-//TODOS LOS CUADRADOS DE LA CUADRICULA
+//CUADRICULA INICIAL
 
 //CUADRADO1
 var cuadrado1 = document.createElement('div');
@@ -184,7 +189,6 @@ BOTON1.addEventListener('click', ()=>{
     cuadrado4.setAttribute("style", "width: 98.6px; height: 98.65px; border:2px solid black");
     cuadrado5.setAttribute("style", "width: 98.6px; height: 98.65px; border:2px solid black");
     cuadrado6.setAttribute("style", "width: 98.6px; height: 98.65px; border:2px solid black");    
-
 
 //SEGUNDA FILA   
     cuadrado7.setAttribute("style", "width: 98.6px; height: 98.65px; border:2px solid black");
@@ -401,203 +405,347 @@ boton2.addEventListener("click", ()=>{
     var cuadrado17 = document.createElement('div');
     cuadrado17.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado17);
+    cuadrado17.addEventListener('mouseover', ()=>{
+    cuadrado17.style.background = "yellow";
+    });
     
     
     var cuadrado18 = document.createElement('div');
     cuadrado18.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado18);
+    cuadrado18.addEventListener('mouseover', ()=>{
+    cuadrado18.style.background = "yellow";
+    });
 
     var cuadrado19 = document.createElement('div');
     cuadrado19.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado19);
+    cuadrado19.addEventListener('mouseover', ()=>{
+    cuadrado19.style.background = "yellow";
+    });
 
     var cuadrado20 = document.createElement('div');
     cuadrado20.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado20);
+    cuadrado20.addEventListener('mouseover', ()=>{
+    cuadrado20.style.background = "yellow";
+    });
     
     var cuadrado21 = document.createElement('div');
     cuadrado21.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado21);
+    cuadrado21.addEventListener('mouseover', ()=>{
+    cuadrado21.style.background = "yellow";
+    });
 
     var cuadrado22 = document.createElement('div');
     cuadrado22.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado22);
+    cuadrado22.addEventListener('mouseover', ()=>{
+    cuadrado22.style.background = "yellow";
+    });
 
     var cuadrado23 = document.createElement('div');
     cuadrado23.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado23);
+    cuadrado23.addEventListener('mouseover', ()=>{
+    cuadrado23.style.background = "yellow";
+    });
 
     var cuadrado24 = document.createElement('div');
     cuadrado24.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado24);
+    cuadrado24.addEventListener('mouseover', ()=>{
+    cuadrado24.style.background = "yellow";
+    });
 
 //CUARTA FILA
 
     var cuadrado25 = document.createElement('div');
     cuadrado25.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado25);
-    
+    cuadrado25.addEventListener('mouseover', ()=>{
+    cuadrado25.style.background = "yellow";
+    });
+
     var cuadrado26 = document.createElement('div');
     cuadrado26.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado26);
+    cuadrado26.addEventListener('mouseover', ()=>{
+    cuadrado26.style.background = "yellow";
+    });
 
     var cuadrado27 = document.createElement('div');
     cuadrado27.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado27);
+    cuadrado27.addEventListener('mouseover', ()=>{
+    cuadrado27.style.background = "yellow";
+    });
 
     var cuadrado28 = document.createElement('div');
     cuadrado28.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado28);
+    cuadrado28.addEventListener('mouseover', ()=>{
+    cuadrado28.style.background = "yellow";
+    });
 
     var cuadrado29 = document.createElement('div');
     cuadrado29.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado29);
+    cuadrado29.addEventListener('mouseover', ()=>{
+    cuadrado29.style.background = "yellow";
+    });
 
     var cuadrado30 = document.createElement('div');
     cuadrado30.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado30);
+    cuadrado30.addEventListener('mouseover', ()=>{
+    cuadrado30.style.background = "yellow";
+    });
 
     var cuadrado31 = document.createElement('div');
     cuadrado31.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado31);
+    cuadrado31.addEventListener('mouseover', ()=>{
+    cuadrado31.style.background = "yellow";
+    });
     
     var cuadrado32 = document.createElement('div');
     cuadrado32.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado32);
+    cuadrado32.addEventListener('mouseover', ()=>{
+    cuadrado32.style.background = "yellow";
+    });
 
 //QUINTA FILA
 
     var cuadrado33 = document.createElement('div');
     cuadrado33.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado33);
+    cuadrado33.addEventListener('mouseover', ()=>{
+    cuadrado33.style.background = "yellow";
+    });
     
     var cuadrado34 = document.createElement('div');
     cuadrado34.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado34);
+    cuadrado34.addEventListener('mouseover', ()=>{
+    cuadrado34.style.background = "yellow";
+    });
 
     var cuadrado35 = document.createElement('div');
     cuadrado35.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado35);
+    cuadrado35.addEventListener('mouseover', ()=>{
+    cuadrado35.style.background = "yellow";
+    });
 
     var cuadrado36 = document.createElement('div');
     cuadrado36.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado36);
+    cuadrado36.addEventListener('mouseover', ()=>{
+    cuadrado36.style.background = "yellow";
+    });
 
     var cuadrado37 = document.createElement('div');
     cuadrado37.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado37);
+    cuadrado37.addEventListener('mouseover', ()=>{
+    cuadrado37.style.background = "yellow";
+    });
 
     var cuadrado38 = document.createElement('div');
     cuadrado38.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado38);
+    cuadrado38.addEventListener('mouseover', ()=>{
+    cuadrado38.style.background = "yellow";
+    });
 
     var cuadrado39 = document.createElement('div');
     cuadrado39.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado39);
+    cuadrado39.addEventListener('mouseover', ()=>{
+    cuadrado39.style.background = "yellow";
+    });
     
     var cuadrado40 = document.createElement('div');
     cuadrado40.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado40);
+    cuadrado40.addEventListener('mouseover', ()=>{
+    cuadrado40.style.background = "yellow";
+    });
 
 
 //SEXTA FILA
     var cuadrado41 = document.createElement('div');
     cuadrado41.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado41);
+    cuadrado41.addEventListener('mouseover', ()=>{
+    cuadrado41.style.background = "yellow";
+    });
     
     var cuadrado42 = document.createElement('div');
     cuadrado42.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado42);
+    cuadrado42.addEventListener('mouseover', ()=>{
+    cuadrado42.style.background = "yellow";
+    });
 
     var cuadrado43 = document.createElement('div');
     cuadrado43.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado43);
+    cuadrado43.addEventListener('mouseover', ()=>{
+    cuadrado43.style.background = "yellow";
+    });
 
     var cuadrado44 = document.createElement('div');
     cuadrado44.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado44);
+    cuadrado44.addEventListener('mouseover', ()=>{
+    cuadrado44.style.background = "yellow";
+    });
 
     var cuadrado45 = document.createElement('div');
     cuadrado45.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado45);
+    cuadrado45.addEventListener('mouseover', ()=>{
+    cuadrado45.style.background = "yellow";
+    });
 
     var cuadrado46 = document.createElement('div');
     cuadrado46.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado46);
+    cuadrado46.addEventListener('mouseover', ()=>{
+    cuadrado46.style.background = "yellow";
+    });
 
     var cuadrado47 = document.createElement('div');
     cuadrado47.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado47);
+    cuadrado47.addEventListener('mouseover', ()=>{
+    cuadrado47.style.background = "yellow";
+    });
     
     var cuadrado48 = document.createElement('div');
     cuadrado48.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado48);
+    cuadrado48.addEventListener('mouseover', ()=>{
+    cuadrado48.style.background = "yellow";
+    });
 
 //SEPTIMA COLUMNA
     var cuadrado49 = document.createElement('div');
     cuadrado49.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado49);
+    cuadrado49.addEventListener('mouseover', ()=>{
+    cuadrado49.style.background = "yellow";
+    });
     
     var cuadrado50 = document.createElement('div');
     cuadrado50.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado50);
+    cuadrado50.addEventListener('mouseover', ()=>{
+    cuadrado50.style.background = "yellow";
+    });
 
     var cuadrado51 = document.createElement('div');
     cuadrado51.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado51);
+    cuadrado51.addEventListener('mouseover', ()=>{
+    cuadrado51.style.background = "yellow";
+    });
 
     var cuadrado52 = document.createElement('div');
     cuadrado52.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado52);
+    cuadrado52.addEventListener('mouseover', ()=>{
+    cuadrado52.style.background = "yellow";
+    });
 
     var cuadrado53 = document.createElement('div');
     cuadrado53.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado53);
+    cuadrado53.addEventListener('mouseover', ()=>{
+    cuadrado53.style.background = "yellow";
+    });
 
     var cuadrado54 = document.createElement('div');
     cuadrado54.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado54);
+    cuadrado54.addEventListener('mouseover', ()=>{
+    cuadrado54.style.background = "yellow";
+    });
 
     var cuadrado55 = document.createElement('div');
     cuadrado55.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado55);
+    cuadrado55.addEventListener('mouseover', ()=>{
+    cuadrado55.style.background = "yellow";
+    });
     
     var cuadrado56 = document.createElement('div');
     cuadrado56.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado56);
+    cuadrado56.addEventListener('mouseover', ()=>{
+    cuadrado56.style.background = "yellow";
+    });
 
 //OCTAVA FILA
     var cuadrado57 = document.createElement('div');
     cuadrado57.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado57);
+    cuadrado57.addEventListener('mouseover', ()=>{
+    cuadrado57.style.background = "yellow";
+    });
     
     var cuadrado58 = document.createElement('div');
     cuadrado58.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado58);
+    cuadrado58.addEventListener('mouseover', ()=>{
+    cuadrado58.style.background = "yellow";
+    });
 
     var cuadrado59 = document.createElement('div');
     cuadrado59.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado59);
+    cuadrado59.addEventListener('mouseover', ()=>{
+    cuadrado59.style.background = "yellow";
+    });
 
     var cuadrado60 = document.createElement('div');
     cuadrado60.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado60);
+    cuadrado60.addEventListener('mouseover', ()=>{
+    cuadrado60.style.background = "yellow";
+    });
 
     var cuadrado61 = document.createElement('div');
     cuadrado61.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado61);
+    cuadrado61.addEventListener('mouseover', ()=>{
+    cuadrado61.style.background = "yellow";
+    });
 
     var cuadrado62 = document.createElement('div');
     cuadrado62.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado62);
+    cuadrado62.addEventListener('mouseover', ()=>{
+    cuadrado62.style.background = "yellow";
+    });
 
     var cuadrado63 = document.createElement('div');
     cuadrado63.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado63);
-    
+    cuadrado63.addEventListener('mouseover', ()=>{
+    cuadrado63.style.background = "yellow";
+    });
+
     var cuadrado64 = document.createElement('div');
     cuadrado64.setAttribute("style", "width: 73px; height: 73px; border:2px solid black");
     cuadricula.appendChild(cuadrado64);    
+    cuadrado64.addEventListener('mouseover', ()=>{
+    cuadrado64.style.background = "yellow";
+    });
 
 });
 
